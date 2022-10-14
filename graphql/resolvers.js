ac960324ba4e0d8cacaf8bfa6248806c1c1fc46b
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-10-14 00:28:33 -0300

    Adding a create post mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,6 +3,7 @@ const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const Post = require("../models/post");
 
 module.exports = {
   // You need a method for every query/mutation you define in your schema
@@ -66,5 +67,40 @@ module.exports = {
       { expiresIn: "1h" } // Here we stablish the token will expire and be invalid in 1hour
     );
     return { token: token, userId: user._id.toString() };
+  },
+  createPost: async ({ postInput }, req) => {
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: "Title is invalid." });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: "Content is invalid." });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+      // creator: req.userId, // This will be a string not an object but mongoose will convert it for us
+    });
+    const createdPost = await post.save();
+    // Add post to users posts
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString(),
+    };
   },
 };
